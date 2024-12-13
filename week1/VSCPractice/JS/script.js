e5c9de5f987491435b42fcc68321f495e78ae406
Belete Misgan Mengie <154594574+BeleteGM@users.noreply.github.com>
2024-12-12 22:47:14 -0900

    phase1 evangadi project

---
+++ week1/VSCPractice/JS/script.js
@@ -0,0 +1,13 @@
+1, JavaScript var keyword
+2, JavaScript let keyword
+3, JavaScript const keyword 
+Note: In JavaScript, variables can be declared automatically.
+Syntax
+// Declaration using var
+var geek = "Hello Geek"
+ 
+// Declaration using let      
+let $ = "Welcome"                  
+ 
+// Declaration using const   
+const _example = "Gfg"      
---
+++ week6/AppleBootstrapProject/project/js/apple.js
@@ -0,0 +1,56 @@
+// // const express= require('express');
+// // const app =express();
+// // app.listen(80, ()=>{
+// //     console.log("it is listining");
+
+// // })
+// // // app.get('/', function(req, res){
+// // // res.end("danile is full stack website developer");
+// // // })
+// // // app.get('/index.html', function(req, res){
+// // //     res.end("belete  is full stack website developer");
+// // // })
+// // app.use(express.static('project'));
+// // const path = requrie('path');
+// // console.log(path.parse(__filename));
+// const path = require("path"); // importing path module
+// const os = require("os");
+// // const parsedFileName = path.parse(__dirname);
+// // console.log(parsedFileName.name);
+// const filename= path.parse(__filename);
+// console.log(filename.name); // use the parse method
+
+// // will print name of your file, i.e.,r
+
+// let freemem21= os.freemem();
+// console.log(freemem21);
+
+// const fs = require('fs')
+
+// // directory full path
+// const dir = '.aynalem/mulu/tomas/yisak';
+
+// // recursively create multiple directories
+// fs.mkdir(dir, { recursive: true }, err => {
+//   if (err) {
+//     throw err
+//   }
+
+
+//   console.log('Directory is created.')
+// })
+
+// fs.readFile('myList.txt', 'utf8', (err, data) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     console.log(data);
+//   });
+
+
+const http= require("http");
+console.log(http);
+const server= crea
+
+
